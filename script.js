af07f923245f289d1488264f74e35817753a5780
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-03-08 14:08:40 -0800

    can change todos in the console

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ todoList = {
     todos.forEach(function(todo) {
       console.log(todo);
     });
-  }
+  },
   addTodos: function(todo) {
   todos.push(todo)
   todoList.displayTodos();
-  }
+  },
+  changeTodos: function(position, newTodo) {
+  todos[position] = newTodo
+  todoList.displayTodos();
+}
+
 };
